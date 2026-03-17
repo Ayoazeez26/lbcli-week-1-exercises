476711e7c2697d83aa7f23f23939a3ea16a51e79
Ayoazeez26 <44169294+Ayoazeez26@users.noreply.github.com>
2026-03-17 16:23:01 +0100

    feat: mined blocks

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,3 @@
 # Check the total amount in the wallet.
-  bitcoin-cli -regtest getbalance
+  bitcoin-cli -regtest generatetoaddress 101 "$ADDR" > /dev/null
+  bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
